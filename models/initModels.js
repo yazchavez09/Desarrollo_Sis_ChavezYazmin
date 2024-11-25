02d87415fd71de836fb7d5f3d7cca1016d21c2d1
Yazmin Chavez <yazmin.chavez.et32@gmail.com>
2024-11-25 08:24:41 -0300

    FK

--- models/initModels.js
+++ models/initModels.js
@@ -1,4 +1,3 @@
-// models/initModels.js
 const Rol = require('../models/SQL_Rol');
 const Persona = require('../models/SQL_Persona');
 const Usuario = require('../models/SQL_Usuario');
@@ -7,34 +6,40 @@ const Producto = require('../models/SQL_Productos');
 const Facturacion = require('../models/SQL_Facturacion');
 const Carrito = require('../models/SQL_Carrito');
 const Item_carrito = require('../models/SQL_Item_carrito');
-const Proveedores = require('../SQL_Proveedores');
-const Egresos = require('../SQL_Egresos');
-
+const Proveedores = require('../models/SQL_Proveedores');
+const Egresos = require('../models/SQL_Egresos');
 
 const initModels = () => {
     // Relación entre Usuario y Rol
-    Rol.hasOne(Usuario);
-    Usuario.belongsTo(Rol);
+    Rol.hasOne(Usuario, { foreignKey: 'id_rol' });
+    Usuario.belongsTo(Rol, { foreignKey: 'id_rol' });
 
     // Relación entre Persona y Usuario
-    Persona.hasOne(Usuario);
-    Usuario.belongsTo(Persona);
+    Persona.hasOne(Usuario, { foreignKey: 'dni_persona' });
+    Usuario.belongsTo(Persona, { foreignKey: 'dni_persona' });
 
-    Persona.hasOne(Cliente);
-    Cliente.belongsTo(Persona);
+    // Relación entre Persona y Cliente
+    Persona.hasOne(Cliente, { foreignKey: 'dni_cliente' });
+    Cliente.belongsTo(Persona, { foreignKey: 'dni_cliente' });
 
-    Producto.hasMany(Item_carrito);
-    Item_carrito.belongsTo(Producto);
+    // Relación entre Producto y Item_carrito
+    Producto.hasMany(Item_carrito, { foreignKey: 'id_producto' });
+    Item_carrito.belongsTo(Producto, { foreignKey: 'id_producto' });
 
-    Carrito.hasMany(Item_carrito);
-    Item_carrito.belongsTo(Carrito);
+    // Relación entre Carrito y Item_carrito
+    Carrito.hasMany(Item_carrito, { foreignKey: 'id_carrito' });
+    Item_carrito.belongsTo(Carrito, { foreignKey: 'id_carrito' });
 
-    Facturacion.hasMany(Carrito);
-    Carrito.belongsTo(Facturacion);
+    // Relación entre Facturación, Cliente y Carrito
+    Facturacion.belongsTo(Cliente, { foreignKey: 'id_cliente' });
+    Cliente.hasMany(Facturacion, { foreignKey: 'id_cliente' });
 
-    Proveedores.hasMany(Egresos);
-    Egresos.belongsTo(Proveedores);
-    
+    Facturacion.belongsTo(Carrito, { foreignKey: 'id_carrito' });
+    Carrito.hasOne(Facturacion, { foreignKey: 'id_carrito' });
+
+    // Relación entre Proveedores y Egresos
+    Proveedores.hasMany(Egresos, { foreignKey: 'dni_proveedor' });
+    Egresos.belongsTo(Proveedores, { foreignKey: 'dni_proveedor' });
 };
 
 // Llama a la función de inicialización para definir las relaciones
